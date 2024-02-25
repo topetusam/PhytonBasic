fc2a4e492aaea72275d4c3039d4305a5dc99dbf8
topetusam <67940360+topetusam@users.noreply.github.com>
2024-02-24 23:27:50 -0500

    paroimpar

---
+++ Paroimpar.py
@@ -0,0 +1,10 @@
+Num1 = int(input("ingrese el numero"))
+
+resultado= Num1%2
+# % muestra el residuo los numero impares al dividirlos por 2 dejan un residuo por lo cual podemos compararlo
+
+if resultado == 0:
+    print("El numero es par")
+else:
+    print("el numero es impar")
+    
