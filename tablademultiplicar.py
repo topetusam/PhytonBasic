721001089881eaa1471849cc50f3f70da418d3f1
topetusam <67940360+topetusam@users.noreply.github.com>
2024-02-25 15:42:44 -0500

    Tablamultiplicar

---
+++ tablademultiplicar.py
@@ -0,0 +1,20 @@
+Num1 = int(input("Ingresa el numero que desea saber su tabla de multiplicar : "))
+
+print("La tabla de multiplicar va del 1 al 10")
+
+
+for i in range(1, 11):
+    if Num1 ==0:
+        print("El resultado es 0")      
+    else:
+        tablamultiplicar= Num1*i
+        print("El resultado es", tablamultiplicar)
+        
+    
+    
+   
+    
+        
+            
+            
+    
