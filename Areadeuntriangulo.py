d149007b1f88f9671bc4e316e4ac3cf28a2a8b88
topetusam <67940360+topetusam@users.noreply.github.com>
2024-02-24 23:37:32 -0500

    areadeuntriangulo

---
+++ Areadeuntriangulo.py
@@ -0,0 +1,9 @@
+Base = float(input("Ingrese la base del triangulo :"))
+Altura = float(input("ingrese la altura del triangulo :"))
+
+
+AreaTriangulo= (Base*Altura)/2
+
+
+print("El area del Triangulo es :", AreaTriangulo)
+ 
